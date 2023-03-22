bc95679090e7a590d12a54a2b40dcf5edfef5f64
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-22 13:17:28 -0700

    addTwoNumbers

---
+++ DCC-2.js
@@ -0,0 +1,23 @@
+/*-----------------------------------------------------------------
+Challenge: 02-addTwoNumbers
+Difficulty: Basic  
+Prompt:
+Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
+If either argument is not a Number, return the value of NaN.
+Examples:
+addTwoNumbers(5, 10) //=> 15
+addTwoNumbers(10, -2) //=> 8
+addTwoNumbers(0, 0) //=> 0
+addTwoNumbers('Hello', 5) //=> NaN
+-----------------------------------------------------------------*/
+// Your solution for 02-addTwoNumbers here:
+
+addTwoNumbers = (num1, num2) => {
+  if (num1 === Nan || num2 === Nan){
+    return "NaN"
+  } else {
+  return num1 + num2
+  }
+}
+
+console.log(addTwoNumbers( 3, 7) //=> 10
