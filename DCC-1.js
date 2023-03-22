62e791d991fbf438c8b748909d378971791a2734
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-22 13:13:10 -0700

    AddOne

---
+++ DCC-1.js
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+
+addOne = (num) => {
+return num + 1
+}
+
+console.log(addOne(5)) // => 6
