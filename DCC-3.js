3427a187f783f6ae0e1b36f42e7eb64110847cd9
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-22 13:24:26 -0700

    sumNumbers

---
+++ DCC-3.js
@@ -0,0 +1,22 @@
+/*-----------------------------------------------------------------
+Challenge: 03-sumNumbers
+Difficulty: Basic  
+Prompt:
+- Write a function called sumNumbers that accepts a single array of numbers and returns the sum of the numbers in the array.
+- If the array is empty, return 0 (zero).
+Examples:
+sumNumbers([10]) //=> 10
+sumNumbers([5, 10]) //=> 15
+sumNumbers([2, 10, -5]) //=> 7
+sumNumbers([]) //=> 0
+-----------------------------------------------------------------*/
+// Your solution for 03-sumNumbers here:
+
+let sum = 0
+sumNumbers = (arr) => {
+  for(num in arr){
+    return sum += arr[i] 
+  }
+}
+
+console.log(sumNumbers([5, 3, 2]) // => 10
