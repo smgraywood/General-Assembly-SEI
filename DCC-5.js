896c71383a495128c905b0fb091b1a8d4c27e508
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-22 13:51:02 -0700

    computeRemainder

---
+++ DCC-5.js
@@ -0,0 +1,25 @@
+/*-----------------------------------------------------------------
+Challenge: 05-computeRemainder
+Difficulty: Basic
+Prompt:
+- Write a function named computeRemainder that accepts two numeric arguments and returns the remainder of the division of those two numbers.
+- The first argument should be the dividend and the second argument should be the divisor.
+- If a 0 is passed in as the second argument you should return JavaScript's special numeric value: Infinity.
+- For extra fun, complete this challenge without using the modulus (%) operator.
+Examples:
+computeRemainder(10,2) //=> 0
+computeRemainder(4,0) //=> Infinity
+computeRemainder(10.5, 3) //=> 1.5
+-----------------------------------------------------------------*/
+// Your solution for 05-computeRemainder:
+
+computeRemainder = (num1, num2) => {
+  if (num2 === 0){
+   return Infinity 
+  }
+  else{
+   return num1 % num2
+  }
+}
+
+console.log(computeRemainder(10, 3)) // => 1
