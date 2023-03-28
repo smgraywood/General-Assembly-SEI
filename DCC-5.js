c5d9d537db93bb96271b5d20e885e90fecc9bf8d
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-28 09:31:41 -0700

    computeRemainder

--- DCC-5.js
+++ DCC-5.js
@@ -13,7 +13,7 @@ computeRemainder(10.5, 3) //=> 1.5
 -----------------------------------------------------------------*/
 // Your solution for 05-computeRemainder:
 
-computeRemainder = (num1, num2) => {
+const computeRemainder = (num1, num2) => {
   if (num2 === 0){
    return Infinity 
   }
