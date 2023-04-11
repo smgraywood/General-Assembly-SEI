d4415c94addbe9111eccf5a707bf38c3bc130d43
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-04-11 08:30:32 -0700

    hammingDistance

---
+++ DCC-12.js
@@ -0,0 +1,27 @@
+// Prompt:
+
+// In information theory, the hamming distance refers to the count of the differences between two strings of equal length.  It is used in computer science for such things as implementing "fuzzy search"  capability.
+
+// - Write a function named hammingDistance that accepts two arguments which are both strings of equal length.
+// - The function should return the count of the symbols (characters, numbers, etc.) at the same position within each string that are different.
+// - If the strings are not of the same length, the function should return NaN.
+
+// Examples:
+
+// hammingDistance('abc', 'abc'); //=> 0
+// hammingDistance('a1c', 'a2c'); //=> 1
+// hammingDistance('!!!!', '****'); //=> 4
+// hammingDistance('abc', 'ab'); //=> NaN
+
+const hammingDistance = (str1, str2) => {
+	if (str1 !== str2) {
+		return NaN;
+	}
+	let diffCount = 0;
+	for (let i = 0; i < str1.length; i++) {
+		if (str1[i] !== str2[i]) {
+			diffCount++;
+		}
+	}
+	return diffCount;
+};
